27117d52e0fba426a2f58f633578482e4bf390bd
dimitrios <sedzinfo@gmail.com>
2025-02-09 15:27:18 +0200

    Update generate_package.R

--- generate_package.R
+++ generate_package.R
@@ -6,7 +6,8 @@
 # R CMD build melirt --resave-data
 library(devtools)
 library(roxygen2)
-setwd(paste0(gsub("generate_package.R","",rstudioapi::getActiveDocumentContext()$path)))
+directory<-paste0(gsub("generate_package.R","",rstudioapi::getActiveDocumentContext()$path))
+setwd(directory)
 # usethis::create_package("melirt")
 # usethis::use_data_raw(name='data/mfi.rda')
 # file.create("R/data.R")
